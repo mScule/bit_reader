f6ca47044842158f59946b60686cbef66acebfca
mScule <vilhelmniemi@hotmail.com>
2021-12-05 13:46:37 +0200

    bit_reader 0.1.1 getchar() removed from the end of the program. Little bit of code refactoring, and updated test.bat file

--- test/test.bat
+++ test/test.bat
@@ -1,8 +1,9 @@
-start ../bin/bit_reader.exe ^
+"../bin/bit_reader.exe" ^
 
   -e little ^
-  -o helloworld.exe ^
+  -o output.exe ^
 
   helloworld_x86_windows.txt
 
+  @echo Open the output.exe in hex editor to see that it is the same program as in the hex dump file!
 pause
